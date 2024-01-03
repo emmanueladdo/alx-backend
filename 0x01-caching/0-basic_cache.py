891a3d12920bc705ee330965c51776bd3e5094c7
emmanueladdo <asememmanueladd@gmail.com>
2024-01-03 17:03:29 +0000

    the basic cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -4,6 +4,7 @@ Class the Inherits from BaseCaching
 """
 from base_caching import BaseCaching
 
+
 class BasicCache(BaseCaching):
     """
     BasicCache class that inherits from BaseCaching
@@ -13,11 +14,10 @@ class BasicCache(BaseCaching):
         """ Add an item in the cache"""
         if key is not None and item is not None:
             self.cache_data[key] = item
-    
+
     def get(self, key):
         """
         Get an item by key
         """
         if key is not None:
             return self.cache_data.get(key)
-    
