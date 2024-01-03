70873146439b3dd7910736e73a759a4fa8e42d8c
emmanueladdo <asememmanueladd@gmail.com>
2024-01-03 23:50:55 +0000

    Lifo algo

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,16 +1,12 @@
-#!/usr/bin/env python3
-"""
-Class the Inherits from BaseCaching
-for fifo caching
-"""
+#!/usr/bin/python3
+"""LIFOCache class that inherits from BaseCaching"""
 from base_caching import BaseCaching
 
-
 class LIFOCache(BaseCaching):
-    """A Last-in-first-out(LIFO) caching system"""
+    """A Last-In-First-Out (LIFO) caching system"""
 
     def __init__(self):
-        """Initialize FIFOCache"""
+        """Initialize LIFOCache"""
         super().__init__()
         self.stack = []
 
@@ -19,13 +15,12 @@ class LIFOCache(BaseCaching):
         if key is not None and item is not None:
             if key in self.cache_data:
                 self.stack.remove(key)
-            self.stack.append(key)
-            self.cache_data[key] = item
-
-            if len(self.stack) > BaseCaching.MAX_ITEMS:
-                removed_key = self.stack.pop(0)
+            while len(self.stack) >= self.MAX_ITEMS:
+                removed_key = self.stack.pop()
                 self.cache_data.pop(removed_key)
                 print(f'DISCARD: {removed_key}')
+            self.stack.append(key)
+            self.cache_data[key] = item
 
     def get(self, key):
         """Retrieve the value associated with the given key"""
