1e2adb7c107453fbcd785d9b7c43e4cf1eb53353
emmanueladdo <asememmanueladd@gmail.com>
2024-01-03 23:38:19 +0000

    fifo cache1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -22,7 +22,7 @@ class FIFOCache(BaseCaching):
             self.queue.append(key)
             self.cache_data[key] = item
 
-            if len(self.queue) > self.MAX_ITEMS:
+            if len(self.queue) > BaseCaching.MAX_ITEMS:
                 removed_key = self.queue.pop(0)
                 self.cache_data.pop(removed_key)
                 print(f'DISCARD: {removed_key}')
