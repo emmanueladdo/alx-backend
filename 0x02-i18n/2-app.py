41a671955cf275bb99724d08760e5a094a17333f
emmanueladdo <asememmanueladdo@gmail.com>
2024-01-10 23:00:34 +0000

    get local from reqeustS

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -19,10 +19,12 @@ class Config(object):
 
 app.config.from_object(Config)
 
+
 @babel.localeselector
 def get_locale() -> str:
     """uses request accept best match"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route('/')
 def index_page():
