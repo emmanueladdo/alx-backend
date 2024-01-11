61c6deb0435c9312d43f1ae6e9d2dbff53d9c258
emmanueladdo <asememmanueladdo@gmail.com>
2024-01-11 11:13:08 +0000

    mm

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -27,7 +27,7 @@ def get_locale() -> Union[str, None]:
     if 'locale' in request.args:
         # Get the value of the 'locale' parameter
         requested_locale = request.args['locale']
-        
+
         # Check if the requested_locale is a supported language
         if requested_locale in app.config['LANGUAGES']:
             return requested_locale
