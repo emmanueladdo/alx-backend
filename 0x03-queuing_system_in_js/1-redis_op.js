fe8b7ee855676d4375aa3644d4fe4fe3e29f2e1c
emmanueladdo <asememmanueladdo@gmail.com>
2024-02-07 21:14:43 +0000

    1-redis

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -2,26 +2,24 @@ import redis from 'redis';
 
 const client = redis.createClient();
 
-client.on('error', (err) => console.log("Redis client not connected to the server:", err));
-client.on('connect', () => console.log("Redis client connected to the server"));
-
+client.on('error', (err) => console.log('Redis client not connected to the server:', err));
+client.on('connect', () => console.log('Redis client connected to the server'));
 
 const setNewSchool = (schoolName, value) => {
-    client.set(schoolName, value, redis.print);
+  client.set(schoolName, value, redis.print);
 };
 
 const displaySchoolValue = (schoolName) => {
-    client.get(schoolName, (err, reply) => {
-      if (err) {
-        console.error(`Error retrieving value for key ${schoolName}:`, err);
-      } else {
-        console.log(`Value for ${schoolName}:`, reply);
-      }
-    });
-  };
+  client.get(schoolName, (err, reply) => {
+    if (err) {
+      console.error(`Error retrieving value for key ${schoolName}:`, err);
+    } else {
+      console.log(`Value for ${schoolName}:`, reply);
+    }
+  });
+};
 
-
-  //Function cal
+// Function cal
 displaySchoolValue('Holberton');
 setNewSchool('HolbertonSanFrancisco', '100');
 displaySchoolValue('HolbertonSanFrancisco');
