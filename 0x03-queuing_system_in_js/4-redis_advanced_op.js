2131ed09df2d7db0cab1e95db497ffd0cc14d463
emmanueladdo <asememmanueladdo@gmail.com>
2024-02-07 10:56:33 +0000

    Node Redis client and advanced operations

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,27 @@
+import { createClient, print } from 'redis';  // Use named imports
+
+const client = createClient();
+
+client.on('error', (err) => console.log("Redis client not connected to the server:", err));
+client.on('connect', () => console.log("Redis client connected to the server"));
+
+const holbertonSchools = {
+  Portland: 50,
+  Seattle: 80,
+  'New York': 20,
+  Bogota: 20,
+  Cali: 40,
+  Paris: 2
+};
+
+for (const [key, value] of Object.entries(holbertonSchools)) {
+  client.hset('HolbertonSchools', key, value, print);  // Use named print function
+}
+
+client.hgetall('HolbertonSchools', (err, obj) => {
+  if (obj) {
+    console.log(obj);
+  } else {
+    throw err;
+  }
+});
